d8f8cc35db66dcce2ef19698417384f584891ea6
KeaKohv <kohv.kea@gmail.com>
2021-11-02 11:19:06 +0200

    first commit

---
+++ .env/application.py
@@ -0,0 +1,175 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required, usd
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# # Configure CS50 Library to use SQLite database TO DO
+# db = SQL(os.getenv("DATABASE_URL"))
+
+
+@app.route("/")
+#@login_required
+def index():
+    """Show index page"""
+
+    return render_template("index.html")
+
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # # Forget any user_id
+    # session.clear()
+
+    # # User reached route via POST (as by submitting a form via POST)
+    # if request.method == "POST":
+
+    #     # Ensure username was submitted
+    #     if not request.form.get("username"):
+    #         return apology("must provide username", 403)
+
+    #     # Ensure password was submitted
+    #     elif not request.form.get("password"):
+    #         return apology("must provide password", 403)
+
+    #     # Query database for username
+    #     rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+    #     # Ensure username exists and password is correct
+    #     if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+    #         return apology("invalid username and/or password", 403)
+
+    #     # Remember which user has logged in
+    #     session["user_id"] = rows[0]["id"]
+
+    #     # Redirect user to home page
+    #     return redirect("/")
+
+    # # User reached route via GET (as by clicking a link or via redirect)
+    # else:
+    #     return render_template("login.html")
+    return render_template("login.html")
+
+
+@app.route("/password", methods=["GET", "POST"])
+@login_required
+def password():
+    """Change password"""
+    # if request.method == "POST":
+    #     # Ensure old password was submitted
+    #     if not request.form.get("old"):
+    #         return apology("must provide old password", 403)
+    #     # Ensure old password was submitted
+    #     elif not request.form.get("new"):
+    #         return apology("must provide new password", 403)
+    #     # Ensure old password was submitted
+    #     elif not request.form.get("confirmation"):
+    #         return apology("must confirm new password", 403)
+    #     # Ensure new password and confirmation match
+    #     elif request.form.get("new") != request.form.get("confirmation"):
+    #         return apology("password and its confirmation do not match", 403)
+
+    #     # Update password in the database
+    #     db.execute("UPDATE users SET hash = :new_hash WHERE id = :user_id",
+    #                new_hash=generate_password_hash(request.form.get("new")), user_id=session["user_id"])
+
+    #     return redirect("/")
+    # if request.method == "GET":
+    #     return render_template("password.html")
+    return "TO DO"
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # # Forget any user_id
+    # session.clear()
+
+    # # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    # # User reached route via POST (as by submitting a form via POST)
+    # if request.method == "POST":
+    #     # Ensure username was submitted
+    #     if not request.form.get("username"):
+    #         return apology("must provide username", 400)
+
+    #     # See if this username already exists in the database
+    #     rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+    #     # Ensure username does not exist
+    #     if len(rows) != 0:
+    #         return apology("this username already exists", 400)
+
+    #     # Ensure password was submitted
+    #     elif not request.form.get("password"):
+    #         return apology("must provide password", 400)
+
+    #     # Ensure password confirmation was submitted
+    #     elif not request.form.get("confirmation"):
+    #         return apology("must confirm password", 400)
+
+    #     # Ensure password and confirmation match
+    #     elif request.form.get("password") != request.form.get("confirmation"):
+    #         return apology("password and its confirmation do not match", 400)
+
+    #     # Insert username and hashed password into database
+    #     db.execute("INSERT INTO users (username, hash) VALUES (?, ?)",
+    #                request.form.get("username"),  generate_password_hash(request.form.get("password")))
+
+    #     return redirect("/login")
+
+    # # User reached route via GET (as by clicking a link or via redirect)
+    # if request.method == "GET":
+    #     return render_template("register.html")
+    return "TO DO"
+
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
