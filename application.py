5c52be5039fc2434878438691979b5a71f78cc77
KeaKohv <kohv.kea@gmail.com>
2021-11-23 20:51:54 +0200

    fix2

--- application.py
+++ application.py
@@ -244,17 +244,17 @@ def children_sleep_needs(user_id):
             }
             children.append(new_child)
 
-    # for child in children:
-    #     months = child["age_in_months"]
-    #     sleep_needs = db.execute("SELECT * FROM sleep_needs WHERE age_min <= ? AND age_max >= ?",
-    #                               months, months)
+    for child in children:
+        months = child["age_in_months"]
+        sleep_needs = db.execute("SELECT * FROM sleep_needs WHERE age_min <= ? AND age_max >= ?",
+                                  months, months)
        
-    #     if len(sleep_needs) == 1:
-    #         child["total"] = sleep_needs["total"]
-    #         child["naps"] = sleep_needs["naps"]
-    #     else:
-    #         child["total"] = "N/i"
-    #         child["naps"] = "N/i"
+        if len(sleep_needs) == 1:
+            child["total"] = "N/ii"
+            child["naps"] = "N/ii"
+        else:
+            child["total"] = "N/i"
+            child["naps"] = "N/i"
 
     return children
 
